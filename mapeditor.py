55d3c8f5db01b063e0b9e5244af5a7b5bf1a0fb8
Gourab Karmakar <gourabk7458@gmail.com>
2024-08-30 04:03:15 +0530

    Added Grid and Target position HighLight where sprites will be placed in MapEditor

--- mapeditor.py
+++ mapeditor.py
@@ -9,6 +9,7 @@ WINDOW_SIZE = (1000, 800)
 OFFSET = 10
 GRID_SIZE = 32
 MAP_FILE_PATH = "data/maps/"
+TARGET_OFFSET = 15
 
 
 class Game:
@@ -35,31 +36,34 @@ class Game:
         self.currGridMap = "0map.json"
         self.currNonGridMap = "1map.json"
         self.inGridMode = False
+        self.targetCenter = (
+            self.mouseCoord[0] - TARGET_OFFSET,
+            self.mouseCoord[1] - TARGET_OFFSET,
+        )
 
     def loadMap(self, currGridMap, currNonGridMap):
-        with open(MAP_FILE_PATH + currGridMap, 'r') as file:
+        with open(MAP_FILE_PATH + currGridMap, "r") as file:
             g = json.load(file)
-        with open(MAP_FILE_PATH + currNonGridMap, 'r') as file:
+        with open(MAP_FILE_PATH + currNonGridMap, "r") as file:
             ng = json.load(file)
 
         self.gridMap.clear()
         self.nonGridMap.clear()
 
-        for k,v in g.items():
-            keys = (int(k.split(',')[0]), int(k.split(',')[1]))
+        for k, v in g.items():
+            keys = (int(k.split(",")[0]), int(k.split(",")[1]))
             self.gridMap[tuple(keys)] = tuple(v)
-        for k,v in ng.items():
-            keys = (int(k.split(',')[0]), int(k.split(',')[1]))
+        for k, v in ng.items():
+            keys = (int(k.split(",")[0]), int(k.split(",")[1]))
             self.nonGridMap[tuple(keys)] = tuple(v)
 
-
     def saveMap(self):
-        g={}
-        ng={}
-        for k,v in self.gridMap.items():
+        g = {}
+        ng = {}
+        for k, v in self.gridMap.items():
             km = f"{k[0]},{k[1]}"
             g[km] = v
-        for k,v in self.nonGridMap.items():
+        for k, v in self.nonGridMap.items():
             km = f"{k[0]},{k[1]}"
             ng[km] = v
         with open(MAP_FILE_PATH + self.currGridMap, "w") as file:
@@ -73,19 +77,22 @@ class Game:
                 self.sprites[v[0]][v[1]], (k[0] * GRID_SIZE, k[1] * GRID_SIZE)
             )
         for k, v in self.nonGridMap.items():
-            self.window.blit(
-                self.sprites[v[0]][v[1]], k
-            )
+            self.window.blit(self.sprites[v[0]][v[1]], k)
 
     def place(self, pos):
         if self.inGridMode:
-            self.gridMap[(pos[0] // GRID_SIZE, pos[1] // GRID_SIZE)] = (self.spriteType, self.currSprite)
+            self.gridMap[(pos[0] // GRID_SIZE, pos[1] // GRID_SIZE)] = (
+                self.spriteType,
+                self.currSprite,
+            )
         else:
             self.nonGridMap[pos] = (self.spriteType, self.currSprite)
 
     def delete(self, pos):
         if self.inGridMode:
-            self.gridMap.pop((pos[0] // GRID_SIZE, pos[1] // GRID_SIZE), "No such key in map found")
+            self.gridMap.pop(
+                (pos[0] // GRID_SIZE, pos[1] // GRID_SIZE), "No such key in map found"
+            )
         else:
             self.nonGridMap.pop(pos, "No such key in map found")
 
@@ -119,6 +126,73 @@ class Game:
             s.append(loadImgs(v))
         return s
 
+    # high Lighter
+    def genRect(self, pos):
+        gridPercent = int(GRID_SIZE * 0.2)
+        posTL = (pos[0] - (pos[0] % GRID_SIZE) - 1, pos[1] - (pos[1] % GRID_SIZE) - 1)
+        posTRV = (
+            pos[0] - (pos[0] % GRID_SIZE) + GRID_SIZE + 1,
+            pos[1] - (pos[1] % GRID_SIZE) - 1,
+        )
+        posTRH = (
+            pos[0] - (pos[0] % GRID_SIZE) + (GRID_SIZE - gridPercent) + 1,
+            pos[1] - (pos[1] % GRID_SIZE) - 1,
+        )
+        posBLV = (
+            pos[0] - (pos[0] % GRID_SIZE) - 1,
+            pos[1] - (pos[1] % GRID_SIZE) + (GRID_SIZE - gridPercent) + 1,
+        )
+        posBLH = (
+            pos[0] - (pos[0] % GRID_SIZE) - 1,
+            pos[1] - (pos[1] % GRID_SIZE) + GRID_SIZE + 1,
+        )
+        posBRV = (
+            pos[0] - (pos[0] % GRID_SIZE) + GRID_SIZE + 1,
+            pos[1] - (pos[1] % GRID_SIZE) + (GRID_SIZE - gridPercent) + 1,
+        )
+        posBRH = (
+            pos[0] - (pos[0] % GRID_SIZE) + (GRID_SIZE - gridPercent) + 1,
+            pos[1] - (pos[1] % GRID_SIZE) + GRID_SIZE + 1,
+        )
+        topLeftV = pygame.Rect(posTL, (1, gridPercent))
+        topLeftH = pygame.Rect(posTL, (gridPercent, 1))
+        topRightV = pygame.Rect(posTRV, (1, gridPercent))
+        topRightH = pygame.Rect(posTRH, (gridPercent, 1))
+        bottomLeftV = pygame.Rect(posBLV, (1, gridPercent))
+        bottomLeftH = pygame.Rect(posBLH, (gridPercent, 1))
+        bottomRightV = pygame.Rect(posBRV, (1, gridPercent))
+        bottomRightH = pygame.Rect(posBRH, (gridPercent, 1))
+        return (
+            topLeftV,
+            topLeftH,
+            topRightV,
+            topRightH,
+            bottomLeftV,
+            bottomLeftH,
+            bottomRightV,
+            bottomRightH,
+        )
+
+    def renderHighLighter(self):
+        currHighLightSqr = self.genRect(self.mouseCoord)
+        for rec in currHighLightSqr:
+            pygame.draw.rect(self.window, (255, 0, 0), rec)
+
+    def genTargetRect(self, pos):
+        gridPercent = int(GRID_SIZE * 0.8)
+        posV = (pos[0], pos[1] - 12)
+        posH = (pos[0] - 12, pos[1])
+        rectV = pygame.Rect(posV, (1, gridPercent))
+        rectH = pygame.Rect(posH, (gridPercent, 1))
+        return (rectV, rectH)
+
+    def renderTargetHighLighter(self, pos):
+        currTargetRect = self.genTargetRect(pos)
+        for rec in currTargetRect:
+            pygame.draw.rect(self.window, (255, 0, 0), rec)
+
+    # high Lighter
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -127,9 +201,49 @@ class Game:
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:  # left
-                        self.place(self.mouseCoord)
+                        if self.inGridMode:
+                            self.place(self.mouseCoord)
+                        else:
+                            self.place(
+                                (
+                                    self.targetCenter[0]
+                                    - (
+                                        self.sprites[self.spriteType][
+                                            self.currSprite
+                                        ].get_width()
+                                        // 2
+                                    ),
+                                    self.targetCenter[1]
+                                    - (
+                                        self.sprites[self.spriteType][
+                                            self.currSprite
+                                        ].get_height()
+                                        // 2
+                                    ),
+                                )
+                            )
                     if event.button == 3:  # right
-                        self.delete(self.mouseCoord)
+                        if self.inGridMode:
+                            self.delete(self.mouseCoord)
+                        else:
+                            self.delete(
+                                (
+                                    self.targetCenter[0]
+                                    - (
+                                        self.sprites[self.spriteType][
+                                            self.currSprite
+                                        ].get_width()
+                                        // 2
+                                    ),
+                                    self.targetCenter[1]
+                                    - (
+                                        self.sprites[self.spriteType][
+                                            self.currSprite
+                                        ].get_height()
+                                        // 2
+                                    ),
+                                )
+                            )
                     if event.button == 5:  # s_down
                         self.changeSprite(SCROLL_SPEED)
                     if event.button == 4:  # s_up
@@ -153,16 +267,23 @@ class Game:
             self.renderMap()
 
             self.mouseCoord = pygame.mouse.get_pos()
+            self.targetCenter = (
+                self.mouseCoord[0] - TARGET_OFFSET,
+                self.mouseCoord[1] - TARGET_OFFSET,
+            )
+
             if self.inGridMode:
                 self.window.blit(
                     self.getCurrSprite(True),
                     (self.mouseCoord[0] + OFFSET, self.mouseCoord[1] + OFFSET),
                 )
+                self.renderHighLighter()
             else:
                 self.window.blit(
                     self.getCurrSprite(True),
                     (self.mouseCoord[0] + OFFSET, self.mouseCoord[1] + OFFSET),
                 )
+                self.renderTargetHighLighter(self.targetCenter)
 
             pygame.display.update()
             self.clock.tick(60)
